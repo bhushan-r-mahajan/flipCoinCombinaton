73d66143970c2bd28f5976d17eb0f38585358f65
bhushan-r-mahajan <bhushanmahajan1997@gmail.com>
2021-04-21 15:31:42 +0530

    Added loop for flipping coin multiple times and determine percentage for the singlet combination

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,18 +1,33 @@
+
 echo "Welcome"
 tails=0
 heads=1
+headsWin=0
+tailsWin=0
 
 function flipCoin(){
 
 flip=$((RANDOM%2))
-if [ $flip -eq $heads ]
-then
-	echo "Heads"
-elif [ $flip -eq $tails ]
-then
-	echo "Tails"
-fi
 
 }
+read -p "How many times do you want to flip coin? = " n;
+declare -A singletDict
+for ((i=0; i<n; i++))
+do
+	flipCoin
+	if [[ $heads -eq $flip ]]
+	then
+		((headsWin++))
+		singletDict[key$i]=H
+	elif [[ $tails -eq $flip ]]
+	then
+		((tailsWin++))
+		singletDict[key$i]=T
+	fi
+done
 
-flipCoin
+echo "Dictionary For Singlet Combination:-[" ${singletDict[@]} "]"
+headsPercent=`awk "BEGIN{print ($headsWin/$n)*100}"`
+echo "Heads win % = "$headsPercent
+tailsPercent=`awk "BEGIN{print ($tailsWin/$n)*100}"`
+echo "Tails win % = "$tailsPercent
