cf24a2abd081e8d7093323fe7cd2d6c549895d2b
bhushan-r-mahajan <bhushanmahajan1997@gmail.com>
2021-04-21 16:40:47 +0530

    Added triplets and its percentage calculations

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -88,5 +88,91 @@ echo "TT win % = "$ttPercent
 
 }
 
+function triplet(){
+
+declare -A tripletDict
+
+hhh=(1 1 1)
+hth=(1 0 1)
+hht=(1 1 0)
+htt=(1 0 0)
+ttt=(0 0 0)
+tth=(0 0 1)
+tht=(0 1 0)
+thh=(0 1 1)
+
+hhhWin=0
+hthWin=0
+hhtWin=0
+httWin=0
+tttWin=0
+tthWin=0
+thtWin=0
+thhWin=0
+
+for ((k=0; k<n; k++))
+do
+   for ((q=0; q<3; q++))
+   do
+      flipCoin
+      arr[$q]=$flip
+   done
+
+   if [[ ${arr[0]} -eq ${hhh[0]} && ${arr[1]} -eq ${hhh[1]} && ${arr[2]} -eq ${hhh[2]}  ]]
+   then
+      ((hhhWin++))
+      tripletDict[key$k]=HHH
+	elif [[ ${arr[0]} -eq ${hth[0]} && ${arr[1]} -eq ${hth[1]} && ${arr[2]} -eq ${hth[2]}  ]]
+   then
+      ((hthWin++))
+      tripletDict[key$k]=HTH
+   elif [[ ${arr[0]} -eq ${hht[0]} && ${arr[1]} -eq ${hht[1]} && ${arr[2]} -eq ${hht[2]}  ]]
+   then
+      ((hhtWin++))
+      tripletDict[key$k]=HHT
+   elif [[ ${arr[0]} -eq ${htt[0]} && ${arr[1]} -eq ${htt[1]} && ${arr[2]} -eq ${htt[2]}  ]]
+   then
+      ((httWin++))
+      tripletDict[key$k]=HTT
+   elif [[ ${arr[0]} -eq ${thh[0]} && ${arr[1]} -eq ${thh[1]} && ${arr[2]} -eq ${thh[2]}  ]]
+   then
+      ((thhWin++))
+      tripletDict[key$k]=THH
+   elif [[ ${arr[0]} -eq ${tth[0]} && ${arr[1]} -eq ${tth[1]} && ${arr[2]} -eq ${tth[2]}  ]]
+   then
+      ((tthWin++))
+      tripletDict[key$k]=TTH
+   elif [[ ${arr[0]} -eq ${tht[0]} && ${arr[1]} -eq ${tht[1]} && ${arr[2]} -eq ${tht[2]}  ]]
+   then
+      ((thtWin++))
+      tripletDict[key$k]=THT
+   elif [[ ${arr[0]} -eq ${ttt[0]} && ${arr[1]} -eq ${ttt[1]} && ${arr[2]} -eq ${ttt[2]}  ]]
+   then
+      ((tttWin++))
+      tripletDict[key$k]=TTT
+   fi
+done
+
+echo "Dictionary For Triplet Combination:-["${tripletDict[@]}"]"
+hhhPercent=`awk "BEGIN{print ($hhhWin/$n)*100}"`
+echo "HHH win % = "$hhhPercent
+hhtPercent=`awk "BEGIN{print ($hhtWin/$n)*100}"`
+echo "HHT win % = "$hhtPercent
+hthPercent=`awk "BEGIN{print ($hthWin/$n)*100}"`
+echo "HTH win % = "$hthPercent
+httPercent=`awk "BEGIN{print ($httWin/$n)*100}"`
+echo "HTT win % = "$httPercent
+thtPercent=`awk "BEGIN{print ($thtWin/$n)*100}"`
+echo "THT win % = "$thtPercent
+thhPercent=`awk "BEGIN{print ($thhWin/$n)*100}"`
+echo "THH win % = "$thhPercent
+tthPercent=`awk "BEGIN{print ($tthWin/$n)*100}"`
+echo "TTH win % = "$tthPercent
+tttPercent=`awk "BEGIN{print ($tttWin/$n)*100}"`
+echo "TTT win % = "$tttPercent
+
+}
+
 singlet
 doublet
+triplet
