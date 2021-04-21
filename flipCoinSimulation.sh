adeb601b24409efbf86fccb60a69236e0239e0c9
bhushan-r-mahajan <bhushanmahajan1997@gmail.com>
2021-04-21 14:26:59 +0530

    Added coin flip to tell heads or tails

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,1 +1,18 @@
 echo "Welcome"
+tails=0
+heads=1
+
+function flipCoin(){
+
+flip=$((RANDOM%2))
+if [ $flip -eq $heads ]
+then
+	echo "Heads"
+elif [ $flip -eq $tails ]
+then
+	echo "Tails"
+fi
+
+}
+
+flipCoin
